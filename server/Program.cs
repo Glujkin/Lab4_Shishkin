98e7fe7276198e18568e0f7e7b892d5b08d34bb2
Glujkin <Kartoheckajaren@gmail.com>
2026-09-24 21:01:03 +0300

    Commit 9: added menu

--- server/Program.cs
+++ server/Program.cs
@@ -6,5 +6,14 @@ class Program {
         Console.WriteLine("Группа: ИСП-231");
         Console.WriteLine("Дата: " + DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss"));
         Console.WriteLine();
+         while (true) {
+        Console.WriteLine("--- Меню ---");
+        Console.WriteLine("1 - Показать ФИО");
+        Console.WriteLine("2 - Показать группу");
+        Console.WriteLine("3 - Показать дату");
+        Console.WriteLine("4 - Выход");
+        Console.Write("Ваш выбор: ");
+        string choice = Console.ReadLine();
+        }
     }
 }
